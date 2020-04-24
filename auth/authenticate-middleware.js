15f0dff35b37deb26f2e9d3e82500bcfb942e0d7
Ben <bbjberg91@gmail.com>
2020-04-24 11:23:46 -0400

    login/reg/jokes working with auth

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,24 @@
   before granting access to the next middleware/route handler
 */
 
+const jwt = require('jsonwebtoken');
+const secrets = require('../database/secrets.js');
+
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+const token = req.headers.authorization;
+const secret = secrets.jwtSecret;
+if(token){
+jwt.verify(token, secret, (error,decodedToken) =>{
+    if (error) {
+      res.status(401).json({message: 'you shall not pass'});
+    } else{
+      req.decodedToken = decodedToken;
+      next();
+    }
+  });
+} else {
+  res.status(400).json({message: 'please provide creds.'})
+} 
+
+
 };
